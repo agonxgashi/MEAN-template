2b7ff2bd22032823157d663fd66db90df1bcd79c
agonxgashi <agonxgashi@users.noreply.github.com>
2018-04-25 11:53:09 +0200

    Initial commit

---
+++ repo/example-repo.js
@@ -0,0 +1,19 @@
+// --- example-repo.js ---
+// • This is an example on how a simple mongoose Schema should be created
+// • Mongoose is used to make communication with MongoDB easy and simple
+// -----------------------------------------------------------------------------
+
+const mongoose   = require('mongoose');
+const Schema   = mongoose.Schema;
+
+// • Create Schema. This will be used later to define model fields (db columns)
+const ExampleSchema = new Schema({
+    title  : String,
+    content: String
+});
+
+// • Created Model below will help us to work with MongoDB easily.
+var ExampleModel = mongoose.model('Example', ExampleSchema);
+
+// • Export Model
+module.exports = ExampleModel;
---
+++ server/routes/example-route.js
@@ -0,0 +1,55 @@
+// --- example-route.js ---
+// • This is an example on how a simple route should be created
+// • Routes are imported on index.js file and are used to declare our simple 
+// API endpoints.
+// • Remember, this is a simple template and I will not complicate things but 
+// is a good practice to create one file on which you'll import all routes
+// and import that file later on index.js. This way, index.js file will be
+// more clean and simple to read.
+// -----------------------------------------------------------------------------
+
+const Example = require('./../../repo/example-repo');
+const express = require('express');
+const router = express.Router();
+
+// • Declaring GET method
+router.get('/', function(req, res) {
+
+    // • Use mongoose to get all `examples` in our database
+    // • How we got this find() method you'll ask? Well, that comes from our
+    // declared mongoose model.
+    Example.find(function(err, examples) {
+
+        // • If there is an error, send the error. nothing after res.send(err) 
+        // will execute
+        if (err)
+            res.send(err);
+
+        // • Return all `examples` in JSON format
+        res.json(examples); // return all examples in JSON format
+    });
+});
+
+// • Declaring POST method
+router.post('/', function(req, res) {
+
+    // • Create and save `example` on MongoDB. 
+    // • We get information form request body
+    Example.create({
+        title  : req.body.title,
+        content: req.body.content
+    }, function(err, examples) {
+        if (err)
+            res.send(err);
+
+        // • Get and return all the `examples` after you create another
+        Example.find(function(err, examples) {
+            if (err)
+                res.send(err);
+            res.json(examples);
+        });
+    });
+});
+
+// • Export router to use it on other modules
+module.exports = router;
