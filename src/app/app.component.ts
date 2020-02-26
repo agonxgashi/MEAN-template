70b78620a681423bd265eeaf84b08978dcdb203a
Agon Gashi <agonxgashi@gmail.com>
2020-02-26 10:29:07 +0100

    Upgrade to Angular 9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mean-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MEAN template';
+  note = 'Client app is running!';
 }
