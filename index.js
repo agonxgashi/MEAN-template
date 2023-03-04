b26c497de8fd1af186a554b987201f373e9d1444
Agon Gashi <agonxgashi@gmail.com>
2023-03-04 22:10:17 +0100

    Upgrade mongoose (v7.0.0)

--- index.js
+++ index.js
@@ -14,56 +14,52 @@ const app = express()
 const MONGO_DB = 'mongodb://127.0.0.1/MEAN-Template-app'
 const PORT = 3000
 
-// • Connect to MongoDB database. Please be sure you have started MongoDB
-// services before running application and replace `MEAN-Template-app` with your
-// database's name.
-mongoose.connect(MONGO_DB, (err) => {
 
-  if (err) {
-    // We want to log if app can not connect to database
-    console.log('Could not connect to mongodb')
-    console.log(err)
-  } else {
-    // If there is no error during db connection, continue proccess
-    console.log('Connected to the database!')
+try {
 
-    // • `/dist` is default file output of ng build command. You can change
-    // that on `angular-cli.json` config file but don't forget to change below line
-    // too or server will not be able to locate our front-end part of application.
-    app.use(express.static(path.join(__dirname, 'dist')))
+  // • Connect to MongoDB database. Please be sure you have started MongoDB
+  // services before running application and replace `MEAN-Template-app` with your
+  // database's name.
+  mongoose.connect(MONGO_DB)
+    .catch(() => { console.log('Could not connect to mongodb'); })
 
-    // • This is a special method called `middleware`. Every request will be
-    // executed on each request. If you want to exclude a specific route to make it
-    // not enter on this middleware, simply declare that route before this function
-    app.use('/', function (req, res, next) {
-      // • Implement your logic here.
-      console.log('Time:', Date.now())
-      next()
+  // • `/dist` is default file output of ng build command. You can change
+  // that on `angular-cli.json` config file but don't forget to change below line
+  // too or server will not be able to locate our front-end part of application.
+  app.use(express.static(path.join(__dirname, 'dist')))
+
+  // • This is a special method called `middleware`. Every request will be
+  // executed on each request. If you want to exclude a specific route to make it
+  // not enter on this middleware, simply declare that route before this function
+  app.use('/', function (req, res, next) {
+    // • Implement your logic here.
+    console.log('Time:', Date.now())
+    next()
+  })
+
+  // • We call use() on the Express application to add the Router to handle path,
+  // specifying an URL path on first parameter '/api/example'.
+  app.use('/api/example', require('./server/routes/example-route'))
+
+  // • Every other route that starts with `api/` but not declared above will
+  // return `not-found` status. Apply your `not-found` format here.
+  app.get('/api/*', (req, res) => {
+    res.send({
+      message: 'Endpoint not found',
+      type: 'error'
     })
+  })
 
-    // • We call use() on the Express application to add the Router to handle path,
-    // specifying an URL path on first parameter '/api/example'.
-    app.use('/api/example', require('./server/routes/example-route'))
+  // • Every other route not declared above will redirect us to Angular view
+  // called `index.html`. Be sure you have builded and created output files from
+  // angular app.
+  app.get('*', (req, res) => {
+    console.log(req.url)
+    res.sendFile(path.join(__dirname, 'dist/index.html'))
+  })
 
-    // • Every other route that starts with `api/` but not declared above will
-    // return `not-found` status. Apply your `not-found` format here.
-    app.get('/api/*', (req, res) => {
-      res.send({
-        message: 'Endpoint not found',
-        type: 'error'
-      })
-    })
-
-    // • Every other route not declared above will redirect us to Angular view
-    // called `index.html`. Be sure you have builded and created output files from
-    // angular app.
-    app.get('*', (req, res) => {
-      console.log(req.url)
-      res.sendFile(path.join(__dirname, 'dist/index.html'))
-    })
-
-    // • Start listening on port {{PORT}} for requests.
-    app.listen(PORT, () => console.log(`Application started successfully on port: ${PORT}!`))
-
-  }
-})
+  // • Start listening on port {{PORT}} for requests.
+  app.listen(PORT, () => console.log(`Application started successfully on port: ${PORT}!`))
+} catch (error) {
+  console.log(error)
+}
